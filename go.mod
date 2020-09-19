bb78b3d847184ef7f1f27f2afe8ebb7f95814044
Pierre Prinetti <pierreprinetti@redhat.com>
2020-09-19 17:49:03 +0200

    Remove "parse OS_CLOUD" functionality
    Before this patch, the OS_CLOUD environment variable was used if
    '-authurl' was missing.
    
    Removing this feature, useless in production settings, improves
    behaviour predictability.

--- go.mod
+++ go.mod
@@ -2,8 +2,4 @@ module github.com/shiftstack/os-proxy
 
 go 1.14
 
-require (
-	github.com/BurntSushi/xdg v0.0.0-20130804141135-e80d3446fea1
-	github.com/gofrs/uuid v3.3.0+incompatible
-	gopkg.in/yaml.v2 v2.3.0
-)
+require github.com/gofrs/uuid v3.3.0+incompatible
--- main.go
+++ main.go
@@ -16,16 +16,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-	"os"
 
-	"github.com/BurntSushi/xdg"
 	"github.com/shiftstack/os-proxy/proxy"
-	"gopkg.in/yaml.v2"
 )
 
 const (
@@ -37,61 +32,15 @@ var (
 	osAuthURL      = flag.String("authurl", "", "OpenStack entrypoint (OS_AUTH_URL)")
 )
 
-func getAuthURL(cloudName string) (string, error) {
-	cloudsPath, err := xdg.Paths{XDGSuffix: "openstack"}.ConfigFile("clouds.yaml")
-	if err != nil {
-		return "", err
-	}
-
-	cloudsFile, err := os.Open(cloudsPath)
-	if err != nil {
-		return "", err
-	}
-	defer cloudsFile.Close()
-
-	var clouds struct {
-		Clouds map[string]struct {
-			Auth struct {
-				URL string `yaml:"auth_url"`
-			} `yaml:"auth"`
-		} `yaml:"clouds"`
-	}
-
-	cloudsContent, err := ioutil.ReadAll(cloudsFile)
-	if err != nil {
-		return "", err
-	}
-
-	if err := yaml.Unmarshal(cloudsContent, &clouds); err != nil {
-		return "", err
-
-	}
-
-	if _, ok := clouds.Clouds[cloudName]; !ok {
-		return "", fmt.Errorf("cloud %q not found", cloudName)
-	}
-
-	return clouds.Clouds[cloudName].Auth.URL, nil
-}
-
 func main() {
 	flag.Parse()
 
 	if *proxyURLstring == "" {
-		log.Fatal("Missing required --proxyurl parameter")
+		log.Fatal("Missing required --proxyurl parameter.")
 	}
 
 	if *osAuthURL == "" {
-		if osCloud := os.Getenv("OS_CLOUD"); osCloud != "" {
-			log.Print("Missing --authurl parameter; parsing from clouds.yaml")
-			var err error
-			*osAuthURL, err = getAuthURL(osCloud)
-			if err != nil {
-				log.Fatalf("Error parsing auth_url from cloud %q: %v", osCloud, err)
-			}
-		} else {
-			log.Fatal("Missing --authurl parameter. Set it or set the relative OS_CLOUD environment to enable parsing it from clouds.yaml.")
-		}
+		log.Fatal("Missing required --authurl parameter.")
 	}
 
 	var proxyURL *url.URL
