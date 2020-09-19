9f253e5edf12a874c74fab44845b852f62f0b883
Pierre Prinetti <pierreprinetti@redhat.com>
2020-09-19 12:53:25 +0200

    License under Apache 2.0

--- generate_cert.go
+++ generate_cert.go
@@ -1,6 +1,7 @@
 // Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file at
+// https://github.com/golang/go/blob/master/LICENSE
 
 // Generate a self-signed X.509 certificate for a TLS server. Outputs to
 // 'cert.pem' and 'key.pem' and will overwrite existing files.
